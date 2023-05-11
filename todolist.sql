455a3fc8fdb509541b49b7b212a2a6f03d8ecb07
Marc144 <55266116+Marc144@users.noreply.github.com>
2023-05-11 18:13:52 -0400

    Add files via upload

--- todolist.sql
+++ todolist.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE todolist;
-
-CREATE TABLE  entries (
+CREATE DATABASE IF NOT EXISTS todolist;
+USE todolist;
+CREATE TABLE IF NOT EXISTS  entries (
         what_to_do    VARCHAR(160),
         due_date      VARCHAR(60),
         status      VARCHAR(60)
